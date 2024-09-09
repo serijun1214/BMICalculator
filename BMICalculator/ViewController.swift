28b49ab04dd4a295a17aaf5614492a2f90352aae
serijun1214 <serijun.1214+github@gmail.com>
2024-09-10 05:15:46 +0900

    BMIの計算を実装

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightTextField.placeholder = "身長をcmで入力してください"
+        weightTextField.placeholder = "体重をkgで入力してください"
+        bmiLabel.text = "BMI計算結果"
     }
 
-
+    @IBAction func calcButtonAction(_ sender: Any) {
+        let doubleH = Double(heightTextField.text!)
+        let doubleW = Double(weightTextField.text!)
+        bmiLabel.text = calclation(height: doubleH!, weight: doubleW!)
+    }
+    
+    func calclation(height: Double, weight: Double) -> String {
+        let h = height / 100
+        let w = weight
+        var result = w / (h * h)
+        result = floor(result * 10) / 10
+        
+        return result.description
+    }
+    
 }
 
